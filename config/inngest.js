bfea954ecbd149c79c13d014f4928915d51c8952
icharley <eng.charlesegwuh@gmail.com>
2025-05-27 10:41:53 -0700

    re-imported User from "@/models/User" by taking away {} {user}

--- config/inngest.js
+++ config/inngest.js
@@ -1,6 +1,6 @@
 import { Inngest } from "inngest";
 import connectDB from "./db";
-import { User } from "@models/User";
+import User from "@/models/User";
 // import Order from "@/models/Order";
 
 export const inngest = new Inngest({ id: "ecommerce-class" });
